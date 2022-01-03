f5d9846eeb40fb6177e12f20ba3d526024039007
MaksymTerekhovskyy <santabar6arr@gmail.com>
2022-01-03 20:15:38 +0200

    changed sql script

--- SQLScripts/initDB.sql
+++ SQLScripts/initDB.sql
@@ -5,31 +5,31 @@ CREATE TABLE IncomeCategory
 (
 	id bigint primary key,
 	name character(30) not null unique
-)
+);
 
 CREATE TABLE User_
 (
 	id bigint primary key,
 	login character(15) not null unique,
 	password character(40) not null
-)
+);
 
 CREATE TABLE Income
 (
 	id bigint primary key,
-	name character not null,
+	name character(30) not null,
 	userId bigint not null references User_(id),
 	amount money not null,
 	categoryId bigint not null references IncomeCategory(id),
 	addTime timestamp not null,
 	expirationTime timestamp
-)
+);
 
 CREATE TABLE ListState
 (
 	id bigint primary key,
 	name character(30) not null unique 
-)
+);
 
 CREATE TABLE List
 (
@@ -37,26 +37,26 @@ CREATE TABLE List
 	userId bigint not null references User_(id),
 	name character(30) not null,
 	stateId bigint not null references ListState(id)
-)
+);
 
 CREATE TABLE Coownership
 (
 	userId bigint not null unique references User_(id),
 	listId bigint not null unique references List(id)
-)
+);
 
 CREATE TABLE ProductCategory
 (
 	id bigint primary key,
-	name haracter(30) not null unique 
-)
+	name character(30) not null unique 
+);
 
 CREATE TABLE Product
 (
 	id bigint primary key,
 	listId bigint not null unique references List(id),
 	categoryId bigint not null unique references ProductCategory(id),
-	name haracter(30) not null,
+	name character(30) not null,
 	price money not null,
 	amount bigint not null,
 	productPriority integer not null,
@@ -64,4 +64,4 @@ CREATE TABLE Product
 	addDate timestamp not null,
 	purchaseDate timestamp,
 	buyUntilDate timestamp
-)
+);
