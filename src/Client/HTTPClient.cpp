84f86965aa7bcb7f90ab70d7f89316ac9ded1864
VasylDobrovlskyi <vasyldobrovolski200@gmail.com>
2022-01-11 13:20:01 +0200

    Minnor fixes

--- src/Client/HTTPClient.cpp
+++ src/Client/HTTPClient.cpp
@@ -15,44 +15,20 @@
 #include "Net/Error.h"
 #include "HTTPClient.h"
 
-void HTTPClient::setToken(const std::string& str_token)
+void HTTPClient::set_token(const std::string& str_token)
 {
     m_token = str_token;
 }
 
-Poco::Net::HTTPRequest HTTPClient::getJsonRequest(const Net::Request &net_request)
+void HTTPClient::FormRequest(Poco::Net::HTTPRequest &request,
+                             Poco::Net::HTTPClientSession &session,
+                             const Net::Request &net_request)
 {
-    Poco::URI uri(net_request.uri);
-    std::string path(uri.getPathAndQuery());
-    Poco::Net::HTTPRequest request(net_request.method, path, Poco::Net::HTTPMessage::HTTP_1_1);
-    QByteArray byte_array = net_request.json_playload.toJson();
-    request.setContentLength(byte_array.size());
     request.setContentType(net_request.content_type);
-    return request;
-}
-
-Net::Response HTTPClient::getNetResponse(const Poco::Net::HTTPResponse& response,
-                                          std::istream &received_stream)
-{
-    Net::Response net_response;
-    net_response.content_type = response.getContentType();
-    net_response.status = response.getStatus();
-    net_response.reason = response.getReason();
-    std::stringstream ss;
-    Poco::StreamCopier::copyStream(received_stream, ss);
-    QByteArray br = QString::fromStdString(ss.str()).toUtf8();
-    QJsonDocument json_doc = QJsonDocument::fromJson(br);
-    net_response.json_playload = json_doc;
-    return net_response;
-}
-
-Net::Response HTTPClient::SendRequest(const Net::Request& net_request)
-{
-    Poco::URI uri(net_request.uri);
-    Poco::Net::HTTPClientSession session(uri.getHost(), uri.getPort());
     if (net_request.content_type == Net::CONTENT_TYPE_APPLICATION_JSON)
     {
-        Poco::Net::HTTPRequest request = getJsonRequest(net_request);
+        QByteArray byte_array = net_request.json_playload.toJson();
+        request.setContentLength(byte_array.size());
         std::ostream& o = session.sendRequest(request);
         o << net_request.json_playload.toJson().constData();
     }
@@ -62,22 +38,44 @@ Net::Response HTTPClient::SendRequest(const Net::Request& net_request)
         ;
     else
     {
-        throw Net::BadRequestError("Unsupported content type");
+        throw ("Unsupported content type");
     }
+}
 
-    Poco::Net::HTTPResponse response;
-    std::istream &received_stream = session.receiveResponse(response);
-    std::string response_content_type = response.getContentType();
-    if (response_content_type == Net::CONTENT_TYPE_APPLICATION_JSON)
+Net::Response HTTPClient::FormResponse(const Poco::Net::HTTPResponse& response,
+                                          std::istream &received_stream)
+{
+    Net::Response net_response;
+    net_response.content_type = response.getContentType();
+    net_response.status = response.getStatus();
+    net_response.reason = response.getReason();
+    std::stringstream ss;
+    Poco::StreamCopier::copyStream(received_stream, ss);
+    if (net_response.content_type == Net::CONTENT_TYPE_APPLICATION_JSON)
     {
-        return getNetResponse(response, received_stream);
+        QByteArray br = QString::fromStdString(ss.str()).toUtf8();
+        QJsonDocument json_doc = QJsonDocument::fromJson(br);
+        net_response.json_playload = json_doc;
     }
-    else if (response_content_type == Net::CONTENT_TYPE_PLAIN_TEXT)
-    ;
-    else if (response_content_type == Net::CONTENT_TYPE_EMPTY)
-    ;
+    else if (net_response.content_type == Net::CONTENT_TYPE_PLAIN_TEXT)
+        ;
+    else if (net_response.content_type == Net::CONTENT_TYPE_EMPTY)
+        ;
     else
     {
-        throw Net::InternalError("Unsupported content type");
+        throw ("Unsupported content type");
     }
+    return net_response;
+}
+
+Net::Response HTTPClient::SendRequest(const Net::Request& net_request)
+{
+    Poco::URI uri(net_request.uri);
+    Poco::Net::HTTPClientSession session(uri.getHost(), uri.getPort());
+    std::string path(uri.getPathAndQuery());
+    Poco::Net::HTTPRequest request(net_request.method, path, Poco::Net::HTTPMessage::HTTP_1_1);
+    FormRequest(request, session, net_request);
+    Poco::Net::HTTPResponse response;
+    std::istream &received_stream = session.receiveResponse(response);
+    return FormResponse(response, received_stream);
 }
--- src/Client/HTTPClient.h
+++ src/Client/HTTPClient.h
@@ -3,17 +3,20 @@
 #include <string>
 
 #include <Poco/Net/HTTPRequest.h>
-
+#include <Poco/URI.h>
+#include <Poco/Net/HTTPClientSession.h>
 #include "Net/Message.h"
 
 class HTTPClient
 {
 public:
     HTTPClient(){};
-    void setToken(const std::string& str_token);
+    void set_token(const std::string& str_token);
     Net::Response SendRequest(const Net::Request& net_request);
-    Poco::Net::HTTPRequest getJsonRequest(const Net::Request& net_request);
-    Net::Response getNetResponse(const Poco::Net::HTTPResponse& response,
+    void FormRequest(Poco::Net::HTTPRequest& request,
+                     Poco::Net::HTTPClientSession& session,
+                     const Net::Request& net_request);
+    Net::Response FormResponse(const Poco::Net::HTTPResponse& response,
                                   std::istream &received_stream);
 private:
     std::string m_token;
