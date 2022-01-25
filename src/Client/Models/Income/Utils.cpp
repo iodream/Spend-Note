57965db9b22381e796e51c263494bb712e5693ac
MaksymTerekhovskyy <santabar6arr@gmail.com>
2022-01-25 20:04:47 +0200

    multiplied amount x100 so that it will be easily read as cents in server

--- src/Client/Models/Income/Utils.cpp
+++ src/Client/Models/Income/Utils.cpp
@@ -2,12 +2,16 @@
 
 Income ParseIncome(const QJsonObject& json)
 {
+	long long tmp;
 	Income income;
 
 	SafeReadId(json, "income_id", income.income_id);
 	SafeReadId(json, "owner_id", income.owner_id);
 	SafeReadQString(json, "name", income.name);
-	SafeReadNumber(json, "amount", income.amount);
+
+	SafeReadId(json, "amount", tmp); //currently reading amount as Money (long long)
+	income.amount = tmp/100;
+
 	SafeReadId(json, "category_id", income.category_id);
 	SafeReadQString(json, "add_time", income.add_time);
 	SafeReadQString(json, "expiration_time", income.expiration_time);
@@ -22,7 +26,7 @@ QJsonDocument FormatIncome(const Income& income)
 	json["income_id"] = std::to_string(income.income_id).c_str();
 	json["owner_id"] = std::to_string(income.owner_id).c_str();
 	json["name"] = income.name;
-	json["amount"] = income.amount;
+	json["amount"] = income.amount*100;
 	json["category_id"] = std::to_string(income.category_id).c_str();
 	json["add_time"] = income.add_time.toStdString().c_str();
 	json["expiration_time"] = income.expiration_time.toStdString().c_str();
