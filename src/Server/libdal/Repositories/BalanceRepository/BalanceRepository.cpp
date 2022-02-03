cdba1ed867e679d7a961d42c104c92afc3966fc2
Yaroslav Kernitskyi <yaroslav.kernitskyi@gmail.com>
2022-02-03 10:20:34 +0200

    Renamed constants in BalanceRepository

--- src/Server/libdal/Repositories/BalanceRepository/BalanceRepository.cpp
+++ src/Server/libdal/Repositories/BalanceRepository/BalanceRepository.cpp
@@ -1,21 +1,6 @@
 #include "BalanceRepository.h"
 
-namespace
-{
-	const std::string INCOME_TABLE_NAME = "Income";
-	const std::string INCOME_AMOUNT_FIELD = "amount";
-	const std::string INCOME_USER_ID_FIELD = "userId";
-
-	const std::string PRODUCT_TABLE_NAME = "Product";
-	const std::string PRODUCT_AMOUNT_FIELD = "amount";
-	const std::string PRODUCT_PRICE_FIELD = "price";
-	const std::string PRODUCT_IS_BOUGHT_FIELD = "isBought";
-	const std::string PRODUCT_LIST_ID_FIELD = "listId";
-
-	const std::string LIST_TABLE_NAME = "List";
-	const std::string LIST_ID_FIELD = "id";
-	const std::string LIST_USER_ID_FIELD = "userId";
-}
+#include "DatabaseNames.h"
 
 BalanceRepository::BalanceRepository(pqxx::connection& db_connection) : m_db_connection(db_connection)
 {
@@ -27,15 +12,15 @@ Money BalanceRepository::CalculateBalance(IdType user_id)
 	{
 		pqxx::work w{m_db_connection};
 		auto total_income_row = w.exec1(
-			"SELECT SUM(" + INCOME_AMOUNT_FIELD + ")" +
-			" FROM " + INCOME_TABLE_NAME +
-			" WHERE " + INCOME_USER_ID_FIELD + " = " + w.quote(user_id) + ";");
+			"SELECT SUM(" + db::income::AMOUNT + ")" +
+			" FROM " + db::income::TABLE_NAME +
+			" WHERE " + db::income::USER_ID + " = " + w.quote(user_id) + ";");
 
 		auto total_outcome_row = w.exec1(
-			"SELECT SUM(" + PRODUCT_AMOUNT_FIELD + " * " + PRODUCT_PRICE_FIELD + ") " +
-			"FROM " + PRODUCT_TABLE_NAME + " JOIN " + LIST_TABLE_NAME +
-			" ON " + PRODUCT_TABLE_NAME + "." + PRODUCT_LIST_ID_FIELD + " = " + LIST_TABLE_NAME + "." + LIST_ID_FIELD +
-			" WHERE " + LIST_USER_ID_FIELD + " = " + w.quote(user_id) + " AND " + PRODUCT_IS_BOUGHT_FIELD + ";");
+			"SELECT SUM(" + db::product::AMOUNT + " * " + db::product::PRICE + ") " +
+			"FROM " + db::product::TABLE_NAME + " JOIN " + db::list::TABLE_NAME +
+			" ON " + db::product::TABLE_NAME + "." + db::product::LIST_ID + " = " + db::list::TABLE_NAME + "." + db::list::ID +
+			" WHERE " + db::list::USER_ID + " = " + w.quote(user_id) + " AND " + db::product::IS_BOUGHT + ";");
 
 		w.commit();
 
@@ -53,15 +38,15 @@ Money BalanceRepository::CalculatePlannedBalance(IdType user_id)
 	{
 		pqxx::work w{m_db_connection};
 		auto total_income_row = w.exec1(
-			"SELECT SUM(" + INCOME_AMOUNT_FIELD + ")" +
-			" FROM " + INCOME_TABLE_NAME +
-			" WHERE " + INCOME_USER_ID_FIELD + " = " + w.quote(user_id) + ";");
+			"SELECT SUM(" + db::income::AMOUNT + ")" +
+			" FROM " + db::income::TABLE_NAME +
+			" WHERE " + db::income::USER_ID + " = " + w.quote(user_id) + ";");
 
 		auto total_outcome_row = w.exec1(
-			"SELECT SUM(" + PRODUCT_AMOUNT_FIELD + " * " + PRODUCT_PRICE_FIELD + ") " +
-			"FROM " + PRODUCT_TABLE_NAME + " JOIN " + LIST_TABLE_NAME +
-			" ON " + PRODUCT_TABLE_NAME + "." + PRODUCT_LIST_ID_FIELD + " = " + LIST_TABLE_NAME + "." + LIST_ID_FIELD +
-			" WHERE " + LIST_USER_ID_FIELD + " = " + w.quote(user_id) + ";");
+			"SELECT SUM(" + db::product::AMOUNT + " * " + db::product::PRICE + ") " +
+			"FROM " + db::product::TABLE_NAME + " JOIN " + db::list::TABLE_NAME +
+			" ON " + db::product::TABLE_NAME + "." + db::product::LIST_ID + " = " + db::list::TABLE_NAME + "." + db::list::ID +
+			" WHERE " + db::list::USER_ID + " = " + w.quote(user_id) + ";");
 
 		w.commit();
 
