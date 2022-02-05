c89d0bcd502af68229a01ea4fa30093775653bf6
MaksymTerekhovskyy <santabar6arr@gmail.com>
2022-02-05 14:19:00 +0200

    implemented model for getting incomes

--- src/Client/Models/Income/GetIncomesModel.h
+++ src/Client/Models/Income/GetIncomesModel.h
@@ -3,7 +3,7 @@
 #include <QJsonDocument>
 #include <QJsonObject>
 
-#include "Net/Tools/Parsers/Income/IncomesJSONParser.h"
+#include "Entities/Parsers.h"
 #include "Net/Constants.h"
 #include "Net/Message.h"
 
@@ -12,11 +12,11 @@ class GetIncomesModel
 public:
 	GetIncomesModel(const std::string& hostname) : m_hostname{hostname} {};
 
-	Net::Request FormRequest(/**/);
-	std::vector<Income<QString>> ParseResponse(const Net::Response response);
+	Net::Request FormRequest(IdType user_id);
+	std::vector<Income> ParseResponse(const Net::Response response);
 private:
 	std::string m_hostname{};
 
-	IncomeArrayJSONParser<Income<QString>> m_parser{};
+	IncomesJSONParser m_parser{};
 };
 
