5145bae535be7f5cef75a9ab8844e9f6ddf25b1b
sorbeee <serhii.lakhman.ki.2020@lpnu.ua>
2022-02-06 23:32:18 +0200

    Added model to remove single product from list

---
+++ src/Client/Models/Product/RemoveProductModel.cpp
@@ -0,0 +1,10 @@
+#include "RemoveProductModel.h"
+
+Net::Request RemoveProductModel::FormRequest(const ProductId& product_id)
+{
+	Net::Request request;
+	request.uri = m_hostname + "incomes/" + std::to_string(product_id.id);
+	request.method = Net::HTTP_METHOD_DELETE;
+	request.content_type = Net::CONTENT_TYPE_EMPTY;
+	return request;
+}
