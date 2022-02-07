6d71a825aead1286b426b89a5a55ca1ffb7bb6c3
MaksymTerekhovskyy <santabar6arr@gmail.com>
2022-02-07 15:23:48 +0200

    made some changes to model for getting lists

--- src/Client/Models/List/GetListsModel.h
+++ src/Client/Models/List/GetListsModel.h
@@ -3,23 +3,23 @@
 #include <vector>
 
 #include <QJsonDocument>
-#include <QJsonArray>
+#include <QJsonObject>
 
+#include "Entities/Parsers.h"
 #include "Net/Constants.h"
 #include "Net/Message.h"
-
-#include "Client/Common.h"
-
-#include "Client/Entities/Entities.h"
 
 class GetListsModel
 {
 public:
 	GetListsModel(const std::string& hostname) : m_hostname{hostname} {}
+
 	Net::Request FormRequest(IdType user_id);
 	std::vector<List> ParseResponse(const Net::Response& response);
 
 private:
-	const std::string& m_hostname;
+	std::string m_hostname;
+
+	ListsJSONParser m_parser{};
 };
 
