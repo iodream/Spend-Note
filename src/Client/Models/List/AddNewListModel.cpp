20b64c352a248fad06334d792b1c698db0a33543
sorbeee <serhii.lakhman.ki.2020@lpnu.ua>
2022-02-07 18:05:59 +0200

    Fix model

--- src/Client/Models/List/AddNewListModel.cpp
+++ src/Client/Models/List/AddNewListModel.cpp
@@ -1,16 +1,16 @@
 #include "AddNewListModel.h"
-#include "../../Entities/Parsers.h"
 
-Net::Request  AddNewListsModel::FormRequest(IdType user_id)
+Net::Request  AddNewListsModel::FormRequest(const List& list)
 {
 	Net::Request request;
-	request.uri = m_hostname + "/users/" + std::to_string(user_id) + "/lists";
+	request.uri = m_hostname + "/users/" + std::to_string(list.owner_id) + "/lists";
 	request.method = Net::HTTP_METHOD_POST;
+	request.content_type = Net::CONTENT_TYPE_APPLICATION_JSON;
+	request.json_payload = QJsonDocument(m_formatter.Format(list));
 	return request;
 }
 
 ListId AddNewListsModel::ParseResponse(const Net::Response& response)
 {
-	ListIdJSONParser id_parser;
-	return id_parser.Parse(response.json_payload.object());
+	return m_parser.Parse(response.json_payload.object());
 }
--- src/Client/Models/List/AddNewListModel.h
+++ src/Client/Models/List/AddNewListModel.h
@@ -3,18 +3,24 @@
 #include <QJsonDocument>
 #include <QJsonObject>
 
-#include "Net/Entities/List/ListId.h"
+
+#include "../../Entities/Parsers.h"
+#include "../../Entities/Formatters.h"
 #include "Net/Constants.h"
 #include "Net/Message.h"
+
 
 class AddNewListsModel
 {
 public:
 	AddNewListsModel(const std::string& hostname) : m_hostname{hostname} {}
-	Net::Request FormRequest(IdType user_id);
+	Net::Request FormRequest(const List& list);
 	ListId ParseResponse(const Net::Response& response);
 
 private:
 	const std::string& m_hostname;
+
+	ListIdJSONParser m_parser;
+	ListJSONFormatter m_formatter;
 };
 
