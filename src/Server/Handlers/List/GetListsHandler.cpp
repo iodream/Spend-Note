bb6a17c71003612fa6b059e553017d0f66e65f31
VasylDobrovlskyi <vasyldobrovolski200@gmail.com>
2022-02-07 19:00:04 +0200

    Changed GetListsHandler

--- src/Server/Handlers/List/GetListsHandler.cpp
+++ src/Server/Handlers/List/GetListsHandler.cpp
@@ -11,20 +11,17 @@ GetListsHandler::GetListsHandler()
 {
 }
 
-std::vector<List> GetListsHandler::ToNetLists(const Lists &lists)
+List GetListsHandler::ToNetList(const db::List &db_list, const db::ListState& state)
 {
-	std::vector<List> net_lists;
 	List list;
-	for (const auto db_list : lists)
-	{
-		list.id = db_list.first.id;
-		list.name = db_list.first.name;
-		list.owner_id = db_list.first.owner_id;
-		list.state.id = db_list.second.id;
-		list.state.name = db_list.second.name;
-		net_lists.push_back(list);
-	}
-	return net_lists;
+	list.id = db_list.id;
+	list.name = db_list.name;
+	list.owner_id = db_list.owner_id;
+	list.state.id = state.id;
+	list.state.name = state.name;
+
+	return list;
+
 }
 
 Net::Response GetListsHandler::AuthHandle(const Net::Request& request)
@@ -33,13 +30,18 @@ Net::Response GetListsHandler::AuthHandle(const Net::Request& request)
 	Q_UNUSED(request);
 
 	auto user_id = std::get<long long>(m_params.Get(Params::USER_ID));
-	auto lists = m_facade->GetAllLists(user_id);
+	auto db_lists = m_facade->GetAllLists(user_id);
 
-	Lists out_dto;
-	for (const db::List& list : lists) {
-		auto state = m_facade->GetListStateById(list.state_id);
-		out_dto.push_back({list, state.value()});
+	std::vector<List> lists;
+	for (const db::List& db_list : db_lists) {
+		auto state = m_facade->GetListStateById(db_list.state_id);
+		if (!state.has_value())
+		{
+			throw InternalError(std::string("No list state with id:") + std::to_string(db_list.state_id));
+		}
+		auto list = ToNetList(db_list, state.value());
+		lists.push_back(list);
 	}
-	auto net_lists = ToNetLists(out_dto);
-	return FormJSONResponse(m_formatter.Format(net_lists));
+
+	return FormJSONResponse(m_formatter.Format(lists));
 }
--- src/Server/Handlers/List/GetListsHandler.h
+++ src/Server/Handlers/List/GetListsHandler.h
@@ -13,15 +13,12 @@
 
 class GetListsHandler : public AuthorizedHandler
 {
-	using ListItem = std::pair<const db::List&, const db::ListState&>;
-	using Lists = std::vector<ListItem>;
 public:
 	GetListsHandler();
 	virtual ~GetListsHandler() override {}
 
 	Net::Response AuthHandle(const Net::Request& request) override;
 private:
-	std::vector<List> ToNetLists(const Lists& lists);
-private:
 	ListsJSONFormatter m_formatter;
+	List ToNetList(const db::List& db_list, const db::ListState& state);
 };
