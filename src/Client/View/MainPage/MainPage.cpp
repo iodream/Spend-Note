7f4ed95cff170424e7909e0f6f51a2fa2f5b8edc
sorbeee <92763294+sorbeee@users.noreply.github.com>
2022-02-16 23:00:59 +0200

    Update src/Client/View/MainPage/MainPage.cpp
    Co-authored-by: Dmytro <42720874+iodream@users.noreply.github.com>

--- src/Client/View/MainPage/MainPage.cpp
+++ src/Client/View/MainPage/MainPage.cpp
@@ -97,7 +97,7 @@ void MainPage::OnLogoutClicked()
 	emit Logout();
 }
 
-void MainPage::ShowBalacne(const Balance& money)
+void MainPage::ShowBalance(const Balance& money)
 {
 	m_ui->CurrentBalance->setText("Current Balance:  " + QString::number(money.balance));
 	m_ui->ProjectedBalance->setText("Predicted balance:  " + QString::number(money.planned_balance));
