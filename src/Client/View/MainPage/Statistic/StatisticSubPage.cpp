20272bf0042ac8d8de4b19da83b0bb54b38564f1
sorbeee <serhii.lakhman.ki.2020@lpnu.ua>
2022-02-22 16:29:31 +0200

    Comented unnesseries lines

--- src/Client/View/MainPage/Statistic/StatisticSubPage.cpp
+++ src/Client/View/MainPage/Statistic/StatisticSubPage.cpp
@@ -20,52 +20,52 @@ StatisticSubPage::~StatisticSubPage()
 
 void StatisticSubPage::FormingChart()
 {
-	QPieSeries *series = new QPieSeries();
-	series->setHoleSize(0.35);
+////	QPieSeries *series = new QPieSeries();
+////	series->setHoleSize(0.35);
 
-	std::vector<QPieSlice*> slices;
+//	//std::vector<QPieSlice*> slices;
 
-	//TEST
-	struct Pie
-	{
-		QString name;
-		int number;
-	};
+//	//TEST
+//	struct Pie
+//	{
+//		QString name;
+//		int number;
+//	};
 
-	Pie pie1{"Products 15%", 15};
-	Pie pie2{"Rent flat 20%", 20};
-	Pie pie3{"Helth 10%", 10};
-	Pie pie4{"Entertainment 30%", 30};
-	Pie pie5{"Bills 15%", 15};
-	Pie pie6{"Subscription 10%", 10};
-	std::vector<Pie> pie;
+//	Pie pie1{"Products 15%", 15};
+//	Pie pie2{"Rent flat 20%", 20};
+//	Pie pie3{"Helth 10%", 10};
+//	Pie pie4{"Entertainment 30%", 30};
+//	Pie pie5{"Bills 15%", 15};
+//	Pie pie6{"Subscription 10%", 10};
+//	std::vector<Pie> pie;
 
 
-	pie.push_back(pie1);
-	pie.push_back(pie2);
-	pie.push_back(pie3);
-	pie.push_back(pie4);
-	pie.push_back(pie5);
-	pie.push_back(pie6);
-	//================================
+//	pie.push_back(pie1);
+//	pie.push_back(pie2);
+//	pie.push_back(pie3);
+//	pie.push_back(pie4);
+//	pie.push_back(pie5);
+//	pie.push_back(pie6);
+//	//================================
 
-	for(const auto& el : pie)
-	{
-		slices.push_back(series->append(el.name, el.number));
-	}
+//	for(const auto& el : pie)
+//	{
+//		//slices.push_back(series->append(el.name, el.number));
+//	}
 
-	QChart *chart = new QChart();
+//	//QChart *chart = new QChart();
 
-	chart->legend()->hide();
-	chart->addSeries(series);
-	chart->setAnimationOptions(QChart::SeriesAnimations);
-	chart->createDefaultAxes();
-	chart->setTitle("Spendings");
-	chart->setTheme(QChart::ChartThemeQt);
+//	chart->legend()->hide();
+//	chart->addSeries(series);
+//	chart->setAnimationOptions(QChart::SeriesAnimations);
+//	chart->createDefaultAxes();
+//	chart->setTitle("Spendings");
+//	chart->setTheme(QChart::ChartThemeQt);
 
-	QChartView *chartView = new QChartView(chart);
-	chartView->setRenderHint(QPainter::Antialiasing);
-	chartView->setParent(m_ui->verticalFrame);
+//	QChartView *chartView = new QChartView(chart);
+//	chartView->setRenderHint(QPainter::Antialiasing);
+//	chartView->setParent(m_ui->verticalFrame);
 
 }
 
