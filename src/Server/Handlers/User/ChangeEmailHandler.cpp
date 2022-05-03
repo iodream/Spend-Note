e886a8fda5a3ae47c0939b2721a345ea07748b64
MaksymTerekhovskyy <santabar6arr@gmail.com>
2022-05-03 13:43:37 +0300

    fixed tabs in handler

--- src/Server/Handlers/User/ChangeEmailHandler.cpp
+++ src/Server/Handlers/User/ChangeEmailHandler.cpp
@@ -7,72 +7,73 @@ ChangeEmailHandler::ChangeEmailHandler()
 
 Net::Response ChangeEmailHandler::AuthHandle(const Net::Request &request)
 {
-    SCOPED_LOGGER;
-    std::optional<db::VerificationCode> verification;
+	SCOPED_LOGGER;
+	std::optional<db::VerificationCode> verification;
 	try
 	{
-        verification = m_facade->GetVerificationCodeByUserId(request.uid);
-    }
+		verification = m_facade->GetVerificationCodeByUserId(request.uid);
+	}
 	catch (const db::DatabaseFailure& e)
 	{
-        return FormErrorResponse(
-            NetError::Status::HTTP_INTERNAL_SERVER_ERROR,
-            "Such verification code was not gotten because database error occurred");
-    }
+		return FormErrorResponse(
+			NetError::Status::HTTP_INTERNAL_SERVER_ERROR,
+			"Such verification code was not gotten because database error occurred");
+	}
 
 	if (!verification.has_value())
 	{
-        return FormErrorResponse(
-            NetError::Status::HTTP_NOT_FOUND,
-            "Verification code not found");
-    }
+		return FormErrorResponse(
+			NetError::Status::HTTP_NOT_FOUND,
+			"Verification code not found");
+	}
 
-    QDateTime current_time(QDateTime::currentDateTime());
+	QDateTime current_time(QDateTime::currentDateTime());
 	auto dto = m_parser.Parse(request.json_payload.object());
 
 	if (current_time > QDateTime::fromString(QString::fromStdString(verification.value().expiration_time), Qt::ISODate))
 	{
 		try
 		{
-            m_facade->RemoveVerificationCode(verification.value().id);
-        }
+			m_facade->RemoveVerificationCode(verification.value().id);
+		}
 		catch (const db::DatabaseFailure& e)
 		{
-            return FormErrorResponse(
-                NetError::Status::HTTP_INTERNAL_SERVER_ERROR,
-                "Such verification code was not removed because database error occurred");
-        }
-        return FormErrorResponse(
-            NetError::Status::HTTP_NOT_FOUND,
-            "Verification code not found");
-    }
+			return FormErrorResponse(
+				NetError::Status::HTTP_INTERNAL_SERVER_ERROR,
+				"Such verification code was not removed because database error occurred");
+		}
+		return FormErrorResponse(
+			NetError::Status::HTTP_NOT_FOUND,
+			"Verification code not found");
+	}
 	else
 	{
-        if (dto.code == verification.value().code) {
+		if (dto.code == verification.value().code) {
 			try
 			{
-                m_facade->UpdateUserEmail(request.uid, dto.email);
-            }
-            catch (const db::DatabaseFailure& e) {
-                return FormErrorResponse(
-                    NetError::Status::HTTP_INTERNAL_SERVER_ERROR,
-                    "Such user was not updated because database error occurred");
-            }
-			try
-			{
-                m_facade->RemoveVerificationCode(verification.value().id);
-            }
+				m_facade->UpdateUserEmail(request.uid, dto.email);
+			}
 			catch (const db::DatabaseFailure& e)
 			{
-                return FormErrorResponse(
-                    NetError::Status::HTTP_INTERNAL_SERVER_ERROR,
-                    "Such verification code was not removed because database error occurred");
-            }
-            return FormEmptyResponse(Poco::Net::HTTPServerResponse::HTTPStatus::HTTP_OK);
-        }
-    }
+				return FormErrorResponse(
+					NetError::Status::HTTP_INTERNAL_SERVER_ERROR,
+					"Such user was not updated because database error occurred");
+			}
+			try
+			{
+				m_facade->RemoveVerificationCode(verification.value().id);
+			}
+			catch (const db::DatabaseFailure& e)
+			{
+				return FormErrorResponse(
+					NetError::Status::HTTP_INTERNAL_SERVER_ERROR,
+					"Such verification code was not removed because database error occurred");
+			}
+			return FormEmptyResponse(Poco::Net::HTTPServerResponse::HTTPStatus::HTTP_OK);
+		}
+	}
 
-    return FormErrorResponse(
-        NetError::Status::HTTP_UNAUTHORIZED,
-        "Wrong verification code");
+	return FormErrorResponse(
+		NetError::Status::HTTP_UNAUTHORIZED,
+		"Wrong verification code");
 }
