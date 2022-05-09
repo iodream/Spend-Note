91ba3e635db6a6c71766dd3433e9f5423598044d
Yaroslav Kernitskyi <yaroslav.kernitskyi@gmail.com>
2022-05-09 12:12:06 +0300

    Fixed model url

--- src/Client/Models/Product/GetProductsForPeriodicProductModel.cpp
+++ src/Client/Models/Product/GetProductsForPeriodicProductModel.cpp
@@ -1,9 +1,9 @@
 #include "GetProductsForPeriodicProductModel.h"
 
-Net::Request GetProductsForPeriodicProductModel::FormRequest(IdType list_id, IdType periodic_product_id)
+Net::Request GetProductsForPeriodicProductModel::FormRequest(IdType periodic_product_id)
 {
 	Net::Request request;
-	request.uri = m_hostname + "/lists/" + std::to_string(list_id) + "/periodic-products/" + std::to_string(periodic_product_id) + "/products";
+	request.uri = m_hostname + "/periodic-products/" + std::to_string(periodic_product_id) + "/products";
 	request.method = Net::HTTP_METHOD_GET;
 	request.content_type = Net::CONTENT_TYPE_EMPTY;
 	return request;
--- src/Client/Models/Product/GetProductsForPeriodicProductModel.h
+++ src/Client/Models/Product/GetProductsForPeriodicProductModel.h
@@ -12,7 +12,7 @@ class GetProductsForPeriodicProductModel
 public:
 	GetProductsForPeriodicProductModel(const std::string& hostname) : m_hostname{hostname} {}
 
-	Net::Request FormRequest(IdType list_id, IdType periodic_product_id);
+	Net::Request FormRequest(IdType periodic_product_id);
 	std::vector<Product> ParseResponse(const Net::Response response);
 private:
 	const std::string& m_hostname{};
